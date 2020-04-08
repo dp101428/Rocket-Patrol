7cf42cf2a160e67b27954fee3c6848443f82ca58
Darcy Phipps <diphipps@ucsc.edu>
2020-04-08 15:46:55 -0700

    Assets and screen scroll done

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -2,4 +2,14 @@ class Menu extends Phaser.Scene{
     constructor (){
         super ("menuScene");
     }
+
+    create(){
+        //shows menu text
+        this.add.text(20,20,"Rocket Patrol Menu");
+
+
+        // launches the next scene (temp)
+        this.scene.start("playScene");
+    }
 }
+
